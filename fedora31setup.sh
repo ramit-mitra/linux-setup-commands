54a385282927ba747ec6a4ad6ec2147e082c4ee5
Ramit Mitra <ramit.mitra@gmail.com>
2020-04-20 22:03:18 +0530

    Adding alternate DesktopEnvironment setups
    Adding alternate DE to the setup process - Elementary Linux Pantheon DE and Deepin DE.
    
    Added meaningful terminal outputs for installation steps

--- fedora31setup.sh
+++ fedora31setup.sh
@@ -1,16 +1,45 @@
 #! /usr/bin/bash
+### Author: Ramit Mitra
+### Usage: This is a script that is to be used for setting up your Fedora system post basic OS installation.
+###        Please run this script as SuperUser to not get annoyed by repetative prompts.
+### Audience: Developers who intend to setup a linux based environment
+### Version tested for: Fedora 31
 
+clear && \
+echo "---------------------------------------------------------" && \
+echo "Clearing obsolete packages and updating existing packages" && \
+echo "---------------------------------------------------------" && \
 sudo dnf clean all && \
 sudo dnf check-update && \
 sudo dnf upgrade -y && \
+echo "---------------------------------------------------------" && \
+echo "Adding support for RPM FUSION & FLATPAK repositories" && \
+echo "---------------------------------------------------------" && \
 sudo rpm -Uvh http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm && \
 sudo rpm -Uvh http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm && \
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo && \
+echo "---------------------------------------------------------" && \
+echo "Adding SNAP store to install snap apps" && \
+echo "---------------------------------------------------------" && \
 sudo dnf install snapd -y && \
-sudo dnf install gnome-tweak-tool libgnome -y && \
+echo "---------------------------------------------------------" && \
+echo "Adding and enabling CRON & ANACRON" && \
+echo "---------------------------------------------------------" && \
 sudo dnf install cronie cronie-anacron -y && \
 sudo dnf install dnf-plugin-system-upgrade rpmconf -y && \
 systemctl enable crond.service && \
 sudo dnf install -y libzip && \
+echo "---------------------------------------------------------" && \
+echo "Adding DNF Automatic to automate upgrade task" && \
+echo ">> https://dnf.readthedocs.io/en/latest/automatic.html" && \
+echo "---------------------------------------------------------" && \
+sudo yum install dnf-automatic -y && \
+systemctl enable --now dnf-automatic-install.timer && \
+systemctl start dnf-automatic-install.timer && \
+systemctl status dnf-automatic-install.timer && \
+echo "---------------------------------------------------------" && \
+echo "Installing SNAP and FLATPAK apps" && \
+echo "---------------------------------------------------------" && \
 sudo ln -s /var/lib/snapd/snap /snap && \
 sudo snap install hello-world && \
 sudo snap install codium --classic && \
@@ -20,16 +49,22 @@ sudo snap install insomnia bluemail && \
 sudo snap install android-studio --classic && \
 sudo snap install snap-store && \
 sudo snap install glimpse-editor && \
+sudo flatpak install flathub com.github.alainm23.planner -y && \
+sudo flatpak install flathub io.botfather.Botfather -y && \
+echo "---------------------------------------------------------" && \
+echo "Adding useful 3rd party plugins for audio & video playback" && \
+echo "---------------------------------------------------------" && \
 sudo dnf install gstreamer1-plugins-base gstreamer1-plugins-good \
     gstreamer1-plugins-ugly gstreamer1-plugins-bad-free \
     gstreamer1-plugins-bad-free gstreamer1-plugins-bad-freeworld \
     gstreamer1-plugins-bad-free-extras ffmpeg -y && \
-sudo dnf install vlc -y && \
-sudo dnf install nodejs task -y && \
+echo "---------------------------------------------------------" && \
+echo "Installing DNF apps and programing languages/dev-tools - rustc, dotnet, nodejs, docker, etc" && \
+echo "---------------------------------------------------------" && \
+sudo dnf install vlc nodejs task -y && \
 sudo dnf copr enable @dotnet-sig/dotnet -y && \
 sudo dnf install dotnet -y && \
 sudo dnf install rust cargo -y && \
-sudo dnf install vlc -y && \
 sudo dnf install tlp tlp-rdw -y && \
 sudo dnf install nuntius -y && \
 sudo systemctl enable tlp -y && \
@@ -49,15 +84,31 @@ sudo dnf config-manager \
     https://download.docker.com/linux/fedora/docker-ce.repo && \
 sudo dnf install docker-ce docker-ce-cli containerd.io -y && \
 sudo dnf install grubby -y && \
-sudo dnf install gnome-tweak-tool && \
-sudo dnf install chrome-gnome-shell && \
 sudo grubby --update-kernel=ALL --args="systemd.unified_cgroup_hierarchy=0" && \
 sudo systemctl start docker && \
 sudo systemctl enable docker && \
 sudo docker run hello-world && \
+echo "---------------------------------------------------------" && \
+echo "Installing GNOME tweak tools" && \
+echo "---------------------------------------------------------" && \
+sudo dnf install gnome-tweak-tool libgnome -y && \
+sudo dnf install chrome-gnome-shell -y && \
 clear && \
+echo "---------------------------------------------------------" && \
+echo "Installing Pantheon DE and Deepin DE (just for fun, not strictly necessary)" && \
+echo "---------------------------------------------------------" && \
+sudo dnf group install 'pantheon desktop' -y && \
+sudo dnf group install deepin-desktop -y && \
+sudo dnf install switchdesk switchdesk-gui -y && \
+clear && \
+echo "---------------------------------------------------------" && \
+echo "Done setting up the system. Autoremoving junk packages." && \
+echo "---------------------------------------------------------" && \
 yum autoremove -y && \
 clear && \
+echo "---------------------------------------------------------" && \
+echo "Validating installed packages and versions" && \
+echo "---------------------------------------------------------" && \
 snap --version && \
 rustc --version && \
 python --version && \
@@ -78,16 +129,15 @@ npm install -g ts-node \
     react \
     http-server && \
 echo "---------------------------------------------------------" && \
-echo "Setting up crazy python packages for rockstars" && \
+echo "Setting up a few crazy python packages for rockstars" && \
 echo "https://developer.fedoraproject.org/tech/languages/python/pygobject.html" && \
 echo "---------------------------------------------------------" && \
 sudo dnf install -y pygobject3 python3-gobject libnotify libnotify-devel \
     python2-notify python2-notify python3-notify2 python3-dbus dbus-x11 && \
 sudo pip install dbus-python Flask requests && \
 echo "---------------------------------------------------------" && \
-echo "Setting up PHP Composer globally" && \
+echo "Setting up PHP & Composer globally" && \
 echo "---------------------------------------------------------" && \
-
 sudo dnf install php php-cli php-gd -y && \
 curl -sS https://getcomposer.org/installer | php && \
 mv composer.phar /usr/local/bin/composer && \
